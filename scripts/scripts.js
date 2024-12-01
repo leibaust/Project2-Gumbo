1a29b24b26714806c53e066d15b5c17217b64572
LEIBRANDT <161556597+leibaust@users.noreply.github.com>
2024-12-01 12:55:16 -0800

    cached commit

--- scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-// Leib's section
-document.getElementById('menuIcon').addEventListener('click', function() {
-    document.getElementById('menu').classList.toggle('open');
-});
-
-//Muskan's section
-
-//Ian's section
